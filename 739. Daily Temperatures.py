5bc56c59bf44531f22b993dd4e312ae8833a88cf
Kotejichizu <n.golawski@wp.pl>
2023-11-21 20:56:17 +0100

    739

--- 739. Daily Temperatures.py
+++ 739. Daily Temperatures.py
@@ -1,22 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
-        maximum = max(temperatures)
         res =[]
-        for idx, temperatureToday in enumerate(temperatures):
-            if temperatureToday == maximum:
-                res.append(0)
-                continue
-            days = 1
-            while days+idx <=len(temperatures):
-                if days+idx >= len(temperatures):
-                    res.append(0)
+        for idx, temperatureToday in enumerate(temperatures, start=1):
+            foundFlag = False
+            for daysElapsed, temp in enumerate(temperatures[idx:], start=1):
+                if temp > temperatureToday:
+                    res.append(daysElapsed)
+                    foundFlag = True
                     break
-                elif temperatures[days + idx] > temperatureToday:
-                    res.append(days)
-                    break
-
-                days += 1
-
+            if not foundFlag: res.append(0)
         return res
 
 
